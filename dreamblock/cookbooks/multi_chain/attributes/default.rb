f09e83918062d6c45a660dc118559a41dcf4befb
avi <tenzer@clara.co.uk>
2018-08-08 14:13:57 +0200

    Added params.dat

--- dreamblock/cookbooks/multi_chain/attributes/default.rb
+++ dreamblock/cookbooks/multi_chain/attributes/default.rb
@@ -1,5 +1,8 @@
 default["temp"]["directory"] = "/tmp"
+default["home"]["directory"] = "/home/vagrant"
 default["software"]["multichain"] = "multichain-1.0.5.tar.gz"
-default["blockchain"]["name"] = "chain1"
+default["blockchain"]["name"] = "mytestchain1"
 default["blockchain"]["ip"] = "172.28.128.1"
 default["blockchain"]["port"] = "2679"
+
+
--- dreamblock/cookbooks/multi_chain/recipes/create.rb
+++ dreamblock/cookbooks/multi_chain/recipes/create.rb
@@ -1,6 +1,24 @@
 bash 'create_blockchain' do
   code <<-EOH
     /usr/local/bin/multichain-util create #{node["blockchain"]["name"]}
+    sudo rm ~/.multichain/#{node["blockchain"]["name"]}/params.dat
+  EOH
+end
+
+template "#{node["home"]["directory"]}/.multichain/#{node["blockchain"]["name"]}/params.dat" do
+
+  source "params.dat.erb"
+  mode "644"
+  owner "root"
+  group "root"
+  variables(
+      :chain_name => node['blockchain']['name']
+  )
+  action :create_if_missing
+end
+
+bash 'initiate_new_chain' do
+  code <<-EOH
     /usr/local/bin/multichaind #{node["blockchain"]["name"]} -daemon
   EOH
 end
