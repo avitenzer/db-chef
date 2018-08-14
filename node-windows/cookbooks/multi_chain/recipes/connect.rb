8185e3b5c50ee3753548bf24817e7b72d3c76094
avi <tenzer@clara.co.uk>
2018-08-14 19:23:38 +0200

    Renamed connect batch file. Updated connect.rb

--- node-windows/cookbooks/multi_chain/recipes/connect.rb
+++ node-windows/cookbooks/multi_chain/recipes/connect.rb
@@ -1,3 +1,5 @@
-execute 'multichain_d' do
-  command "multichaind #{node["blockchain"]["name"]}@#{node["blockchain"]["ip"]}:#{node["blockchain"]["port"]} -daemon"
+powershell_script 'initiate_new_chain' do
+  code <<-EOH
+    multichaind -datadir=#{node['app']['directory']}#{node['data']['directory']} #{node["blockchain"]["name"]}@#{node["blockchain"]["ip"]}:#{node["blockchain"]["port"]} -daemon > #{node['app']['directory']}#{node['data']['directory']}/logs.txt
+  EOH
 end
