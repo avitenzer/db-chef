125bdc269fea6fe27a26ddf57b7cfa8111cc3b9b
avi <tenzer@clara.co.uk>
2018-08-19 18:40:55 +0200

    Renamed scripts

--- node-windows/cookbooks/multi_chain/attributes/default.rb
+++ node-windows/cookbooks/multi_chain/attributes/default.rb
@@ -2,7 +2,7 @@ default["app"]["directory"] = "C:/apps"
 default["data"]["directory"] = "/datadir"
 default["software"]["multichain"] = "multichain-windows-1.0.6.zip"
 default["blockchain"]["name"] = "mytestchain"
-default["blockchain"]["ip"] = "127.0.0.1"
+default["blockchain"]["ip"] = "154.0.164.32"
 default["blockchain"]["port"] = "2679"
 
 
